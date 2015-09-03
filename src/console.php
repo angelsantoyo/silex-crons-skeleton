041892ad22839132b9eea5217b4638a8fa4cbe4f
Thomas Picquet <thomas@sctr.net>
2015-09-03 13:37:40 +0200

    Initial Commit

---
+++ src/console.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Project\Command\CacheClear;
+
+$console = new Application('My Silex Application', 'n/a');
+$console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
+$console->setDispatcher($app['dispatcher']);
+
+$console->add(new CacheClear($app));
+
+return $console;
